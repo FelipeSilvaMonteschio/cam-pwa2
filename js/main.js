3ccc7494ec4fa9ed8e25dc8b895e2514dedce45b
FelipeSilvaMonteschio <felipe.monteschio@estudante.ifms.edu.br>
2023-11-13 13:16:41 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+if('serviceWorker' in navigator){
+    window.addEventListener('load', async () => {
+        try{
+            let reg;
+            reg = await navigator.serviceWorker.register('/sw.js', { type: "module"});
+
+            console.log("service Worker registrada", reg);
+        }catch(err){
+            console.log('service Woker falhou', err)
+        }
+    })
+}
+
+var constraints = {video : {facingMode: "user" }, audio: false}
+
+const cameraView = document.querySelector('#camera--view'),
+    cameraOutput = document.querySelector('#camera--output'),
+    cameraSensor = document.querySelector('#camera--sensor'),
+    cameraTrigger = document.querySelector('#camera--trigger')
+
+function cameraStart() {
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream
+    })
+    .catch(function (error) {
+        console.error("ocorreu um Erro.", error)
+    })
+}
+
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add('taken')
+}
+
+window.addEventListener("load", cameraStart, false)
